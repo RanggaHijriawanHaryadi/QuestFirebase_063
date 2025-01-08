2dabefa723edf28e1efaad8f9e4ab7b060bc1904
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2025-01-08 15:08:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.firebasepam.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.firebasepam.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiMhs().container.mahasiswaRepository)
+        }
+
+    }
+}
+fun CreationExtras.aplikasiMhs(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
+
